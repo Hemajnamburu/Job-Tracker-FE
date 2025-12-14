f8fe1289e0babfc860e119db6450cdd1143c2bd1
Hemajnamburu <namburuhemaj@gmail.com>
2025-12-14 23:38:00 +0530

    BE Url change

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://job-tracker-be-veuh.onrender.com/",
 });
 
 api.interceptors.response.use(
